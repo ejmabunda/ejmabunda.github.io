b5aa14f035b179cc335b2edaa3b493f33fef3254
Junior <mjmabunda0@gmail.com>
2025-08-30 18:38:26 +0200

    feat: remove cursor in title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <div className="relative z-10 flex flex-col items-center justify-center h-screen text-center">
         <h1 className="text-4xl font-['IBM_Plex_Mono'] text-gray-200">
           <span className="text-gray-200">Junior</span>
-          <span className="text-white">▮</span>
+          {/* <span className="cursor"></span> */}
         </h1>
         <p className="mt-4 text-gray-400 font-['inter-300']">
           Final year software engineering student @ WeThinkCode_.
